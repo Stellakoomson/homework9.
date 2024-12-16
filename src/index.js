e24e6ed7eb42e56307c7a01a9957cd29614e1394
Stellakoomson <stellakoomson006@gmail.com>
2024-12-16 13:23:31 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,65 @@
+// to change the heading style
+// document.addeventListener("DOMContentLoaded",()=>)
+
+
+//     {
+// const heading = document.getElementByld("heading");
+// heading.style.backgroundColor = "blue";
+// heading.style.color = "#ffff";
+// heading.style.borderRadius = "10px";
+// const p = document.getElementById
+
+    
+// // to change the text content and styles
+// const paragraph = document.getElementsByClassName("paragraph");
+// if(paragraph.length >= 5){
+//     paragraph[0].style.backgroundColor = "gray";
+//     paragraph[0].innerHTML = "I know now what the DOM is👋👋";
+
+
+
+
+
+// }
+
+ function changeColor(){
+    let mine1=document.getElementById("done");
+    mine1.style.backgroundColor="blue";
+ }
+    let mine =document.getElementById("done");
+    mine.addEventListener("click", changeColor);
+
+function changeColor1(){
+
+    let mine2=document.getElementById("word");
+    mine2.style.backgroundColor="blue";
+}
+    let mine2=document.getElementById("word");
+    mine2.addEventListener("click", changeColor1);
+
+function changeColor2(){
+
+    let mine3=document.getElementById("cool");
+    mine3.style.backgroundColor="blue";
+}
+    let mine3=document.getElementById("cool");
+    mine3.addEventListener("click", changeColor2);
+
+function changeColor3(){
+    let mine4=document.getElementById("send");
+    mine4.style.backgroundColor="blue";
+}
+    let mine4=document.getElementById("send");
+    mine4.addEventListener("click", changeColor3);
+
+
+function changeColor4(){
+
+    let mine5=document.getElementById("book");
+    mine5.style.backgroundColor="blue";
+}
+    let mine5=document.getElementById("book");
+    mine5.addEventListener("click", changeColor4);
+
+
+
